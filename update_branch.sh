38fd6c7d95bf78def6d9e6c3b3a280245a640e93
emacip <emacipbelmonte@gmail.com>
2015-08-25 18:15:42 +0100

    first commit

---
+++ update_branch.sh
@@ -0,0 +1,34 @@
+SERVER_URL="http://localhost:9200"
+
+echo "creating new index branches-development \n"
+curl -XPUT $SERVER_URL/branches-development/ -d '{ 
+    "branches-development" : {
+    "mappings" : {
+      "branch" : {
+        "dynamic" : "false",
+        "properties" : {
+          "business_name" : {
+            "type" : "string"
+          },
+          "coordinates" : {
+            "type" : "geo_point"
+          },
+          "cuisines" : {
+            "type" : "string"
+          },
+          "opening_times" : {
+            "type" : "nested",
+            "properties" : {
+              "closing" : {
+                "type" : "integer"
+              },
+              "opening" : {
+                "type" : "integer"
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}'
