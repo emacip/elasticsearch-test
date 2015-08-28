8201d92b829dab5481e4af75e45927e2460cbd32
emacip <emacipbelmonte@gmail.com>
2015-08-28 10:44:37 +0100

    Correct mapping json

--- update_branch.sh
+++ update_branch.sh
@@ -3,21 +3,32 @@ curl -XDELETE $SERVER_URL/branches-development/
 
  
 echo "creating new index branches-development \n"
-curl -XPUT $SERVER_URL/branches-development/ -d '{ 
-    "mappings" : {
-      "branch" : {
-        "dynamic" : "false",
-        "properties" : {
-          "business_name" : {
-            "type" : "string"
-          },
-          "coordinates" : {
-            "type" : "geo_point"
-          },
-          "cuisines" : {
-            "type" : "string"
-          }       
+curl -XPUT $SERVER_URL/branches-development/ -d '{
+  "mappings": {
+    "branch": {
+      "dynamic": false,
+      "properties": {
+        "business_name": {
+          "type": "string"
+        },
+        "coordinates": {
+          "type": "geo_point"
+        },
+        "cuisines": {
+          "type": "string"
+        },
+        "opening_times": {
+          "type": "nested",
+          "properties": {
+            "closing": {
+              "type": "integer"
+            },
+            "opening": {
+              "type": "integer"
+            }
+          }
         }
       }
-    }  
+    }
+  }
 }'
